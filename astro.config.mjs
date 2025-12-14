f006d10577faf5dc1b8a32d30d1076cc593fe8dc
felix <cuong.m9325@gmail.com>
2025-12-14 14:37:31 +0100

    netlify modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
+
+const isBuild = process.argv.includes('build');
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: isBuild ? netlify() : undefined,
   vite: {
     plugins: [tailwindcss()]
   }
